43ea548bbae6f09e9424ba566691a6b7f0c91d87
Marco Kuidja <kuidjamarco@gmail.com>
2024-07-08 16:33:56 +0100

    feat: #12 added OTP feature to users module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,17 +1,19 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
-import { User, UserSchema } from "./schemas/user.schema";
-import { UsersController } from "./users.controller";
-import { UsersService } from "./users.service";
 
 import { AuthService } from "../auth/auth.service";
 import { Log, LogSchema } from "./schemas/log.schema";
+import { OTP, OTPSchema } from "./schemas/otp.schema";
+import { User, UserSchema } from "./schemas/user.schema";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
       { name: Log.name, schema: LogSchema },
+      { name: OTP.name, schema: OTPSchema },
     ]),
   ],
   controllers: [UsersController],
